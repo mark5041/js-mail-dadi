09e3c7df22195cfe29f2cd9fb79ba9fcebfb2c32
mark5041 <mark.beloso5041@gmail.com>
2021-11-24 18:26:49 +0100

    mail completo originale

--- Mail/script.js
+++ Mail/script.js
@@ -6,17 +6,37 @@ button_submission.addEventListener('click',
         let mail_list = ["gianfranco@gmail.com","arthur@gmail.com","bigmac@gmail.com","alfonsoilcalvo@gmail.com"];
         let mail = document.querySelector(".my-input");
         let esito = document.querySelector(".my-box");
+        let unvalid = true;
 
-        if(!(mail_list.includes(mail.value)))
+        // if(!(mail_list.includes(mail.value)))
+        // {
+        //     mail.placeholder = "utente non riconosciuto";
+        //     mail.classList.add("red");
+        // }
+        // else    
+        // {
+        //     esito.innerHTML = `<span> utente valido </span>`;
+        // }
+
+        for(let i = 0; i < mail_list.length + 1; i++)
+        {
+            if(mail_list[i] == mail.value)
+            {
+                unvalid = false;
+            }
+        }
+
+        if(unvalid == true)
         {
             mail.placeholder = "utente non riconosciuto";
             mail.classList.add("red");
+            console.log("bella");
         }
-        else    
+        else 
         {
             esito.innerHTML = `<span> utente valido </span>`;
+            console.log("ciao");
         }
-
         
     }
 );
